bbdc9337328ca487fabc9762a0f5a06fdfbad21a
bgroveben <bgroveben@gmail.com>
2018-01-11 12:15:06 -0800

    The fundamentals -- lists

---
+++ ViolentPython/CH1/lists.py
@@ -0,0 +1,16 @@
+# The ways of the list and list methods.
+
+portList = []
+portList.append(21)
+portList.append(80)
+portList.append(443)
+portList.append(25)
+print portList
+portList.sort()
+print portList
+pos = portList.index(80)
+print "[+] There are "+str(pos)+" ports to scan before 80."
+portList.remove(443)
+print portList
+cnt = len(portList)
+print "[+] Scanning "+str(cnt)+" Total Ports."
