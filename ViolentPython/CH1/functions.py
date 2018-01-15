be9f05f74beb38778eec473637aa23453dfe7d27
bgroveben <bgroveben@gmail.com>
2018-01-15 02:21:55 -0800

    Update functions.py recipe

--- ViolentPython/CH1/functions.py
+++ ViolentPython/CH1/functions.py
@@ -1,11 +1,11 @@
 # Functions = organized blocks of reusable code.
 # Python functions begin with the def keyword.
-# Continuing with the previous FTP vulnerability-scanning example, let's create
-# a function to perform just the action of connecting to the FTP server and
-# returning the banner.
 
 import socket
 
+# Continuing with the previous FTP vulnerability-scanning example, let's create
+# a function to perform just the action of connecting to the FTP server and
+# returning the banner.
 def retBanner(ip, port):
     try:
         socket.setdefaulttimeout(2)
@@ -14,23 +14,43 @@ def retBanner(ip, port):
         banner = s.recv(1024)
         return banner
     except:
-        # Like that? If it fouls up, just return None.
         return
+
+# The function checkVulns() takes the variable banner as a parameter and then
+# uses it to make a determination of the vulnerability of the server.
+def checkVulns(banner):
+    if 'FreeFloat Ftp Server (Version 1.00)' in banner:
+        print '[+] FreeFloat FTP Server is vulnerable.'
+    elif '3Com 3CDaemon FTP Server (Version 2.0)' in banner:
+        print '[+] 3CDaemon FTP Server is vulnerable.'
+    elif 'Ability Server 2.34' in banner:
+        print '[+] Ability FTP Server is vulnerable.'
+    elif 'Sami FTP Server 2.0.2' in banner:
+        print '[+] Sami FTP Server is vulnerable.'
+    else:
+        print '[-] FTP Server is not vulnerable.'
+    return
 
 def not_dunder_main():
     ip1 = '192.168.95.148'
     ip2 = '192.168.95.149'
+    ip3 = '192.168.95.150'
     port = 21
-
 
     banner1 = retBanner(ip1, port)
     if banner1:
-        print '[+] ' + ip1 + ': ' + banner1
+        print '[+] ' + ip1 + ': ' + banner1.strip('\n')
+        checkVulns(banner1)
     banner2 = retBanner(ip2, port)
     if banner2:
-        print '[+] ' + ip1 + ': ' + banner2
+        print '[+] ' + ip2 + ': ' + banner2.strip('\n')
+        checkVulns(banner2)
+    banner3 = retBanner(ip3, port)
+    if banner3:
+        print '[+] ' + ip3 + ': ' + banner3.strip('\n')
+        checkVulns(banner3)
 
-    print "You need to open and FTP server for this code to run."
+    print "You need to open an FTP server for this code to run."
 
 if __name__ == '__main__':
     not_dunder_main()
