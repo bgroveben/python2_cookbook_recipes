4d6f17d61d863c76c6f4b0e6d2073e44c438f9a3
bgroveben <bgroveben@gmail.com>
2018-01-19 05:01:05 -0800

    Update vulnerability scanner to check all addresses on a subnet

---
+++ ViolentPython/CH1/vuln_checking_script.py
@@ -0,0 +1,40 @@
+# Now our script will test all 254 IP addresses on the 192.168.95.0/24 subnet
+# with the ports offering telnet, SSH, smtp, http, imap, and https services.
+
+import socket
+
+def retBanner(ip, port):
+    try:
+        socket.setdefaulttimeout(2)
+        s = socket.socket()
+        s.connect((ip, port))
+        banner = s.recv(1024)
+        return banner
+    except:
+        return
+
+def checkVulns(banner):
+    if 'FreeFloat Ftp Server (Version 1.00)' in banner:
+        print '[+] FreeFloat FTP Server is vulnerable.'
+    elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
+        print '[+] 3CDaemon FTP Server is vulnerable.'
+    elif 'Ability Server 2.34' in banner:
+        print '[+] Ability FTP Server is vulnerable.'
+    elif 'Sami FTP Server 2.0.2' in banner:
+        print '[+] Sami FTP Server is vulnerable.'
+    else:
+        print '[-] FTP Server is not vulnerable.'
+    return
+
+def main():
+    portList = [21, 22, 25, 80, 110, 43]
+    for x in range(1, 255):
+        ip = '192.168.95.' + str(x)
+        for port in portList:
+            banner = retBanner(ip, port)
+            if banner:
+                print '[+] ' + ip + ': ' + banner
+                checkVulns(banner)
+
+if __name__=='__main__':
+    main()
