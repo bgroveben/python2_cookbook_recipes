7022a83b89d61452a06d2d1eb1f1dab174f2fe1f
bgroveben <bgroveben@gmail.com>
2018-01-23 04:51:24 -0800

    Update vulnerability-checking script

--- ViolentPython/CH1/vuln_checking_script.py
+++ ViolentPython/CH1/vuln_checking_script.py
@@ -1,8 +1,15 @@
-# Now our script will test all 254 IP addresses on the 192.168.95.0/24 subnet
-# with the ports offering telnet, SSH, smtp, http, imap, and https services.
-# This file should behave the same way as 1-vulnScanner.py.
+"""
+We can now reassemble all the various pieces and parts of our Python
+vulnerability-scanning script.
+Do not worry if it appears pseudo-complete, lacking the ability to use threads
+of execution or better command line option parsing.
+We will continue to build upon this script in the following chapter.
+This file should behave the same way as 1-vulnScanner.py.
+"""
 
 import socket
+import os
+import sys
 
 def retBanner(ip, port):
     try:
@@ -14,28 +21,36 @@ def retBanner(ip, port):
     except:
         return
 
-def checkVulns(banner):
-    if 'FreeFloat Ftp Server (Version 1.00)' in banner:
-        print '[+] FreeFloat FTP Server is vulnerable.'
-    elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
-        print '[+] 3CDaemon FTP Server is vulnerable.'
-    elif 'Ability Server 2.34' in banner:
-        print '[+] Ability FTP Server is vulnerable.'
-    elif 'Sami FTP Server 2.0.2' in banner:
-        print '[+] Sami FTP Server is vulnerable.'
-    else:
-        print '[-] FTP Server is not vulnerable.'
-    return
+def checkVulns(banner, filename):
+    f = open(filename, 'r')
+    for line in f.readlines():
+        if line.strip('\n') in banner:
+            print '[+] Server is vulnerable: ' + banner.strip('\n')
 
 def main():
-    portList = [21, 22, 25, 80, 110, 43]
-    for x in range(1, 255):
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print '[-] ' + filename + ' does not exist.'
+            exit(0)
+
+        if not os.access(filename, os.R_OK):
+            print '[-] ' + filename + ' access denied.'
+            exit(0)
+    else:
+        print '[-] Usage: ' + str(sys.argv[0]) + ' <vuln filename>'
+        exit(0)
+
+    portList = [21,22,25,80,110,443]
+    for x in range(147, 150):
         ip = '192.168.95.' + str(x)
         for port in portList:
             banner = retBanner(ip, port)
             if banner:
-                print '[+] ' + ip + ': ' + banner
-                checkVulns(banner)
+                print '[+] ' + ip + ' : ' + banner
+                checkVulns(banner, filename)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
