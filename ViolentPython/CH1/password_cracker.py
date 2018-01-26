8eca3bc6c70e87f4aa45c4a6b383f3126bfeaa96
bgroveben <bgroveben@gmail.com>
2018-01-25 16:14:13 -0800

    Write testPass() method for password cracking program

--- ViolentPython/CH1/password_cracker.py
+++ ViolentPython/CH1/password_cracker.py
@@ -36,3 +36,22 @@ testPass(...)
     message indicating the found password and returns.
     Otherwise, it continues to test every word in the dictionary.
 """
+import crypt
+
+def testPass(cryptPass):
+    salt = cryptPass[0:2]
+    dictFile = open('dictionary.txt', 'r')
+    for word in dictFile.readlines():
+        word = word.strip('\n')
+        cryptWord = crypt.crypt(word, salt)
+        if (cryptWord == cryptPass):
+            print "[+] Found Password: " + word + "\n"
+            return
+    print "[-] Password Not Found.\n"
+    return
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
