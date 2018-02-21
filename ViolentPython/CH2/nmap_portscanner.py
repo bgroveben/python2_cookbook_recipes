0a58ec4aceb2788f42abd7984601c31add7c8575
bgroveben <bgroveben@gmail.com>
2018-02-21 15:36:50 -0800

    Write main method for nmap portscanner

--- ViolentPython/CH2/nmap_portscanner.py
+++ ViolentPython/CH2/nmap_portscanner.py
@@ -12,6 +12,21 @@ def nmapScan(tgtHost, tgtPort):
     nmScan = nmap.PortScanner()
     nmScan.scan(tgtHost, tgtPort)
     state=nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
-    print " [*] " + tgtHost = " tcp/" + tgtPort + " " + state
+    print " [*] " + tgtHost + " tcp/" + tgtPort + " " + state
 
 def main():
+    parser = optparse.OptionParser('Please enter argument options for: ' + '-H <target host> -p <target port>')
+    parser.add_option('-H' , dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='Please specify target port[s] separated by a comma.')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(', ')
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print parser.usage
+        exit(0)
+    for tgtPort in tgtPorts:
+        nmapScan(tgtHost, tgtPort)
+
+
+if __name__ == '__main__':
+    main()
